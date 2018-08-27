e8d31d6d16f26f0914d09a2a88f93ccc775d5146
liweiyuan <liweiyuan@etouch.cn>
2018-08-27 11:52:41 +0800

    enhancement：add hystrix的fallback功能

--- learn-other/spring-boot-hystrix-learn/src/main/java/com/wade/springboothystrixlearn/SpringBootHystrixLearnApplication.java
+++ learn-other/spring-boot-hystrix-learn/src/main/java/com/wade/springboothystrixlearn/SpringBootHystrixLearnApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 文章地址https://www.baeldung.com/introduction-to-hystrix
+ * <p>
+ * 提供对通常通过网络访问的服务的故障和延迟的保护和控制
+ * 停止因某些服务停机而导致的故障级联
+ * 快速失败并迅速恢复
+ * 尽可能优雅地降级
+ * 对故障进行实时监控和指挥中心警报
  */
 @SpringBootApplication
 public class SpringBootHystrixLearnApplication {
