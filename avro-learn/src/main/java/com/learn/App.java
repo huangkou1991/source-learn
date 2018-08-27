3a390a313d046821f92066ba4901450217f83ab1
liweiyuan <liweiyuan@etouch.cn>
2018-08-27 15:27:57 +0800

    enhancement：add avro生成序列化文件

---
+++ avro-learn/src/main/java/com/learn/App.java
@@ -0,0 +1,15 @@
+package com.learn;
+
+/**
+ * Hello world!
+ *
+ * https://www.baeldung.com/java-apache-avro
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
