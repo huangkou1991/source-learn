b55819ecd0d06fde84490ad8d9f0faaab1f6e544
liweiyuan <liweiyuan@etouch.cn>
2018-08-27 18:20:21 +0800

    enhancement：add 延迟队列

--- concurrency-learn/src/main/java/com/learn/delayqueue/package-info.java
+++ concurrency-learn/src/main/java/com/learn/delayqueue/package-info.java
@@ -3,5 +3,9 @@
  * @Date : 2018/8/27 17:47
  * @Description :延迟队列的使用
  * 当消费者想要从队列中获取一个元素时，只有当该特定元素的延迟到期时，它们才能接受它。
+ *
+ *  * https://www.cnblogs.com/barrywxx/p/8525907.html
+ *  https://www.baeldung.com/java-delay-queue
+ *  *
  */
 package com.learn.delayqueue;
