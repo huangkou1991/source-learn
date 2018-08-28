859664ebe6d80377387b51e5735cc745d090f258
liweiyuan <liweiyuan@etouch.cn>
2018-08-28 17:29:31 +0800

    enhancement：add spring-boot-zookeeper

---
+++ spring-boot/spring-boot-zookeeper/src/main/java/com/learn/springbootzookeeper/SpringBootZookeeperApplication.java
@@ -0,0 +1,12 @@
+package com.learn.springbootzookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootZookeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootZookeeperApplication.class, args);
+    }
+}
