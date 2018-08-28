977eac8df11a5111cdca66daa763e4760e651ba1
liweiyuan <liweiyuan@etouch.cn>
2018-08-28 12:01:37 +0800

    enhancement：init netty client

---
+++ netty-learn/netty-springboot-client/src/main/java/com/learn/nettyspringbootclient/NettySpringbootClientApplication.java
@@ -0,0 +1,12 @@
+package com.learn.nettyspringbootclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettySpringbootClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettySpringbootClientApplication.class, args);
+    }
+}
