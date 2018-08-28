6d32f29fbd5014144de2e70997f0f07b74646e3e
liweiyuan <liweiyuan@etouch.cn>
2018-08-28 10:43:05 +0800

    enhancement：add netty admin添加日志模块

--- netty-learn/netty-springboot-admin/src/main/java/com/learn/nettyspringbootadmin/NettySpringbootAdminApplication.java
+++ netty-learn/netty-springboot-admin/src/main/java/com/learn/nettyspringbootadmin/NettySpringbootAdminApplication.java
@@ -2,14 +2,12 @@ package com.learn.nettyspringbootadmin;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableAdminServer
-@Configuration
-@EnableAutoConfiguration
+//@Configuration
+//@EnableAutoConfiguration
 public class NettySpringbootAdminApplication {
 
     public static void main(String[] args) {
