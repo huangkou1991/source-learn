946f824866b806777c0d35294c8d947d03e4320b
liweiyuan <liweiyuan@etouch.cn>
2018-08-28 10:38:29 +0800

    enhancement：add netty监控模块

--- netty-learn/netty-springboot-admin/src/main/java/com/learn/nettyspringbootadmin/NettySpringbootAdminApplication.java
+++ netty-learn/netty-springboot-admin/src/main/java/com/learn/nettyspringbootadmin/NettySpringbootAdminApplication.java
@@ -1,12 +1,22 @@
 package com.learn.nettyspringbootadmin;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableAdminServer
+@Configuration
+@EnableAutoConfiguration
 public class NettySpringbootAdminApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NettySpringbootAdminApplication.class, args);
     }
 }
+
+/**
+ * 配置监控模块
+ */
