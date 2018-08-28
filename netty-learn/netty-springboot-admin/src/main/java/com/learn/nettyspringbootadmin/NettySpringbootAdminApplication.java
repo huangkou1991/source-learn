a29f94c02ab46baffcacbdc6d92ef2517b79e9e8
liweiyuan <liweiyuan@etouch.cn>
2018-08-28 10:25:01 +0800

    enhancement：init netty project

---
+++ netty-learn/netty-springboot-admin/src/main/java/com/learn/nettyspringbootadmin/NettySpringbootAdminApplication.java
@@ -0,0 +1,12 @@
+package com.learn.nettyspringbootadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettySpringbootAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettySpringbootAdminApplication.class, args);
+    }
+}
